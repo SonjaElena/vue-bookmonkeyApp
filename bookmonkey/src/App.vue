2c8588c0935ce595e4e15f079f41cff96fae02ec
SonjaElena <sonja.koppenhoefer@web.de>
2024-04-08 12:42:41 +0200

    feat: router connections

---
+++ bookmonkey/src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link> |
+    <!-- <router-link to="/books">Books</router-link> hier ist path der matcher -->
+    <router-link :to="{ name: 'books' }">Books</router-link>
+    <!-- hier ist name der matcher und man kann den path namen ändern -->
+  </nav>
+  <router-view />
+</template>
---
+++ bookmonkey/src/views/BooksView.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about">
+    <h1>This is the book list</h1>
+  </div>
+</template>
